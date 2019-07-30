3452e826910ad7c6e9a6448e54dca75590abe790
dpoarch <davidpoarch@outlook.com>
2019-07-30 21:17:25 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        items: [
+            { item: 'Intuitive interface for hassle-free transactions' },
+            { item: 'Free set up, flat 10% commission per transaction' },
+            { item: 'Easy way to earn passive income'}
+        ]
+    }
+})
